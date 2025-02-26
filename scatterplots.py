9e0738e132005d6b3f543d28e92f14e05f14189f
Bastian12321 <Basclemmensen@gmail.com>
2025-02-26 23:50:55 +0100

    plots

--- scatterplots.py
+++ scatterplots.py
@@ -1,26 +1,28 @@
 from load import *
 import matplotlib.pyplot as plt
 
-#method for iterating over attributes in dataset 
-# and drawing pairwise scatterplots
-def make_scatterplots(start, end):
-    plt.figure(figsize=(12, 10))
-    for m1 in range(start, end):
-        for m2 in range(start, end):
-            plt.subplot(end - start, end - start, (m1 - start) * (end - start) + (m2 - start) + 1)
-            for c in range(C):
-                class_mask = classLabels == c
-                plt.plot(np.array(X[class_mask, m2]), np.array(X[class_mask, m1]), ".")
-                if m1 == (end - 1):
-                    plt.xlabel(attributeNames[m2])
-                else:
-                    plt.xticks([])
-                if m2 == start:
-                    plt.ylabel(attributeNames[m1])
-                else:
-                    plt.yticks([])          
-    plt.legend(classNames)
-    plt.show()
+#Delete famhist
+X = np.delete(X, 4, axis=1)
+M = M - 1
 
-make_scatterplots(0, 4)
-make_scatterplots(4, M)
+colors = ["blue", "red"]
+plt.figure(figsize=(12, 10))
+for m1 in range(M):
+    for m2 in range(M):
+        plt.subplot(M, M, m1 * M + m2 + 1)
+        for c in range(C):
+            class_mask = classLabels == c
+            plt.plot(np.array(X[class_mask, m2]), np.array(X[class_mask, m1]), ".", color=colors[c], markersize=3)
+            if m1 == M - 1:
+                plt.xlabel(attributeNames[m2])
+            else:
+                plt.xticks([])
+            if m2 == 0:
+                plt.ylabel(attributeNames[m1])
+            else:
+                plt.yticks([])
+
+plt.tight_layout()
+plt.legend(classNames)
+plt.show()
+
