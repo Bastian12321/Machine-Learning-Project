ba65a20866696a8ec0257f35db0f60a4e188bf5f
Chris523432 <chrisldao@gmail.com>
2025-03-06 11:58:22 +0100

    Heatmap

--- correlation.py
+++ correlation.py
@@ -1,6 +1,9 @@
 from load import *
 import numpy as np
+import seaborn as sn
 from dtuimldmtools import similarity
+import matplotlib.pyplot as plt
+import sympy as sp
 
 for i in range(M):
     for j in range(i + 1, M):
@@ -8,3 +11,11 @@ for i in range(M):
         y = np.array([X[:, j]], dtype=np.float64)
         sim = similarity(x, y, "cor")
         print(f"({attributeNames[i]}, {attributeNames[j]}): {sim[0][0]:.2f}")
+
+#Heat map
+corrMatrix = np.corrcoef((X.astype(float)).T, dtype=float)
+corr = sp.Matrix(corrMatrix)
+heatmap = sn.heatmap(data = corrMatrix, cmap='coolwarm', annot=True)
+heatmap.set_yticklabels(attributeNames)
+heatmap.set_xticklabels(attributeNames)
+plt.show()
